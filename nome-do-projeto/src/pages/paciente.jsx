4b6f7c241b376cd9d831761bd66884f4a8c9a53f
bruna <brunaalmeida075@gmail.com>
2023-06-15 20:55:48 -0300

    parte onde mostra as consultas marcadas feito

--- nome-do-projeto/src/pages/paciente.jsx
+++ nome-do-projeto/src/pages/paciente.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Schedule from "../components/paciente/ schedule ";
+import AppointmentsScheduled from "../components/paciente/appointmentsScheduled";
 
 function PatientPage() {
     return(
         <div>
             <h2>Página Principal do Paciente</h2>
-            {Schedule}
+            <Schedule />
+            <AppointmentsScheduled />
         </div>
     )
 }
