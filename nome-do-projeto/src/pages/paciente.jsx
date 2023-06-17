8e8fb4efcf304d557da7e3012a1e67480815c971
bruna <brunaalmeida075@gmail.com>
2023-06-17 17:23:40 -0300

    funções em sass feita

--- nome-do-projeto/src/pages/paciente.jsx
+++ nome-do-projeto/src/pages/paciente.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Schedule from "../components/paciente/ schedule ";
 import AppointmentsScheduled from "../components/paciente/appointmentsScheduled";
 import History from "../components/paciente/history";
+import '../style/page/pagePatient.sass'
 
 function PatientPage() {
     return(
-        <div>
-            <h2>Página Principal do Paciente</h2>
+        <div className="login-container ">
+            <h2 className="title">Página Principal do Paciente</h2>
             <Schedule />
             <AppointmentsScheduled />
             <History />
